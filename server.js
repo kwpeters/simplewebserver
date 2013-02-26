bb4c7b69423d48e524b6924e129ac73d0dfea001
Kevin Peters <kwpeters@gmail.com>
2013-02-26 14:29:29 -0500

    Minor fixes.

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var connect = require('connect');
+
+var portNum = 3000;
+console.log("Serving the www folder on port %d.", portNum);
+
 var app = connect()
           .use(connect.static(__dirname + '/www'))
-          .listen(3000);
+          .listen(portNum);
